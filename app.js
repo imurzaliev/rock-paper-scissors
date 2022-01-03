bc015ae11bf6412692bdda6037e288a4e2404598
imurzaliev <imurzalievi@gmial.com>
2022-01-04 00:07:07 +0300

    Add a div for displaying results and change all of console.logs into DOM methods

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const MOVES = ["Rock", "Paper", "Scissors"];
 let computerScore = 0;
 let userScore = 0;
-let rounds = 1;
+let rounds = 5;
+let userPlay = "";
 
-// rounds = prompt("How many rounds do You want to play?");
-
+const score = document.querySelector("#score");
+const text = document.querySelector("#text");
 const buttons = document.querySelectorAll(".button");
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
-    console.log(playRound(button.value, computerPlay()));
+    userPlay = button.value;
+    playRound(userPlay, computerPlay());
+    score.textContent = `Your Score: ${userScore} Computer Score: ${computerScore}`;
   });
 });
 
@@ -19,34 +22,34 @@ function computerPlay() {
 
 function playRound(userSelection, computerSelection) {
   if (computerSelection === userSelection) {
-    return `computer:${computerSelection} vs you:${userSelection}
+    text.textContent = `computer:${computerSelection} vs you:${userSelection}
     Draw`;
   } else if (computerSelection == "Rock" && userSelection == "Scissors") {
     computerScore++;
-    return `computer:${computerSelection} vs you:${userSelection}
+    text.textContent = `computer:${computerSelection} vs you:${userSelection}
       You Lose! Rock beats Scissors`;
   } else if (computerSelection == "Paper" && userSelection == "Rock") {
     computerScore++;
-    return `computer:${computerSelection} vs you:${userSelection}
+    text.textContent = `computer:${computerSelection} vs you:${userSelection}
       You Lose! Paper beats Rock`;
   } else if (computerSelection == "Scissors" && userSelection == "Paper") {
     computerScore++;
-    return `computer:${computerSelection} vs you:${userSelection}
+    text.textContent = `computer:${computerSelection} vs you:${userSelection}
       You Lose! Scissors beats Paper`;
   } else if (computerSelection == "Scissors" && userSelection == "Rock") {
     userScore++;
-    return `computer:${computerSelection} vs you:${userSelection}
+    text.textContent = `computer:${computerSelection} vs you:${userSelection}
       You Win! Rock beats Scissors`;
   } else if (computerSelection == "Rock" && userSelection == "Paper") {
     userScore++;
-    return `computer:${computerSelection} vs you:${userSelection}
+    text.textContent = `computer:${computerSelection} vs you:${userSelection}
       You Win! Paper beats Rock`;
   } else if (computerSelection == "Paper" && userSelection == "Scissors") {
     userScore++;
-    return `computer:${computerSelection} vs you:${userSelection}
+    text.textContent = `computer:${computerSelection} vs you:${userSelection}
       You Win! Scissors beats Paper`;
   } else {
-    return "There is No Such Move";
+    returntext.textContent = "There is No Such Move";
   }
 }
 
