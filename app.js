ace789fed9634296bf9b169d8b119831f8510845
imurzaliev <imurzalievi@gmial.com>
2022-01-03 17:30:58 +0300

    Create 3 buttons. Add an event listener to the buttons

--- app.js
+++ app.js
@@ -3,50 +3,47 @@ let computerScore = 0;
 let userScore = 0;
 let rounds = 1;
 
-rounds = prompt("How many rounds do You want to play?");
+// rounds = prompt("How many rounds do You want to play?");
+
+const buttons = document.querySelectorAll(".button");
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    console.log(playRound(button.value, computerPlay()));
+  });
+});
 
 function computerPlay() {
   let move = MOVES[Math.floor(Math.random() * 3)];
   return move;
 }
 
-function userPlay() {
-  const userMove = prompt("What is your move? Rock, Paper, Scissors");
-  return capitalizeFirstLetter(userMove);
-}
-
-function capitalizeFirstLetter(string) {
-  string = string.toLowerCase();
-  return string.charAt(0).toUpperCase() + string.slice(1);
-}
-
-function winner(userSelection, computerSelection) {
+function playRound(userSelection, computerSelection) {
   if (computerSelection === userSelection) {
-    return `computer:${computerSelection} vs you:${userSelection} 
+    return `computer:${computerSelection} vs you:${userSelection}
     Draw`;
   } else if (computerSelection == "Rock" && userSelection == "Scissors") {
     computerScore++;
-    return `computer:${computerSelection} vs you:${userSelection} 
+    return `computer:${computerSelection} vs you:${userSelection}
       You Lose! Rock beats Scissors`;
   } else if (computerSelection == "Paper" && userSelection == "Rock") {
     computerScore++;
-    return `computer:${computerSelection} vs you:${userSelection} 
+    return `computer:${computerSelection} vs you:${userSelection}
       You Lose! Paper beats Rock`;
   } else if (computerSelection == "Scissors" && userSelection == "Paper") {
     computerScore++;
-    return `computer:${computerSelection} vs you:${userSelection} 
+    return `computer:${computerSelection} vs you:${userSelection}
       You Lose! Scissors beats Paper`;
   } else if (computerSelection == "Scissors" && userSelection == "Rock") {
     userScore++;
-    return `computer:${computerSelection} vs you:${userSelection} 
+    return `computer:${computerSelection} vs you:${userSelection}
       You Win! Rock beats Scissors`;
   } else if (computerSelection == "Rock" && userSelection == "Paper") {
     userScore++;
-    return `computer:${computerSelection} vs you:${userSelection} 
+    return `computer:${computerSelection} vs you:${userSelection}
       You Win! Paper beats Rock`;
   } else if (computerSelection == "Paper" && userSelection == "Scissors") {
     userScore++;
-    return `computer:${computerSelection} vs you:${userSelection} 
+    return `computer:${computerSelection} vs you:${userSelection}
       You Win! Scissors beats Paper`;
   } else {
     return "There is No Such Move";
@@ -54,9 +51,9 @@ function winner(userSelection, computerSelection) {
 }
 
 function game() {
-  for (i = 0; i < rounds; i++) {
-    console.log(winner(userPlay(), computerPlay()));
-  }
+  // for (i = 0; i < rounds; i++) {
+  console.log(playRound(userPlay(), computerPlay()));
+  // }
   if (computerScore > userScore) {
     console.log(`You Loose!
         Your score is ${userScore} and compures scores is ${computerScore}`);
@@ -68,5 +65,3 @@ function game() {
       Your score is ${userScore} and compures scores is ${computerScore}`);
   }
 }
-
-console.log(game());
